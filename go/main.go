76dc5300219349cbfcc8fefe13f23c61f66a1fa4
Takayuki Miyahara <voyager.3taka28@gmail.com>
2021-08-07 14:27:42 +0900

    feat(go): signal ready http server

--- go/main.go
+++ go/main.go
@@ -1,50 +1,34 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io/ioutil"
-	"net"
+	"io"
 	"net/http"
-	"net/http/httputil"
-	"strings"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
-	listener, err := net.Listen("tcp", "localhost:8888")
-	if err != nil {
-		panic(err)
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
+	http.HandleFunc("/", echoProcess)
+	go http.ListenAndServe(":8888", nil)
+
+	s := <-signals
+	switch s {
+	case syscall.SIGINT:
+		fmt.Println("SIGINT")
+	case syscall.SIGTERM:
+		fmt.Println("SIGTERM")
 	}
-	fmt.Println("Server is running at localhost:8888")
+}
 
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			panic(err)
-		}
-
-		go func() {
-			fmt.Printf("Accept %v\n", conn.RemoteAddr())
-			// read request
-			request, err := http.ReadRequest(bufio.NewReader(conn))
-			if err != nil {
-				panic(err)
-			}
-			dump, err := httputil.DumpRequest(request, true)
-			if err != nil {
-				panic(err)
-			}
-			fmt.Println(string(dump))
-			// write response
-			response := http.Response{
-				StatusCode: 200,
-				ProtoMajor: 1,
-				ProtoMinor: 0,
-				Body:       ioutil.NopCloser(strings.NewReader("Hello World\n")),
-			}
-
-			response.Write(conn)
-			conn.Close()
-		}()
+func echoProcess(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		io.WriteString(w, "Hello World!")
+		break
 	}
 }
